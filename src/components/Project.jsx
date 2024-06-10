c82e6ef1f2197637b37730794c20ffa078ac856d
Dhruba Saha <dhrubasaha08@gmail.com>
2024-06-11 05:15:50 +0530

    CSS Fix

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -55,7 +55,7 @@ const ProjectCard = ({
         )}
       </div>
 
-      <div className={`w-full md:w-2/5 px-6 md:p-16 flex flex-col justify-center ${isEven ? "text-left md:text-left" : "text-left md:text-right"}`}>
+      <div className={`w-full md:w-3/5 px-6 md:p-16 flex flex-col justify-center ${isEven ? "text-left md:text-left" : "text-left md:text-right"}`}>
         <h3 className='text-white font-medium text-md sm:text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl lg:text-5xl leading-tight'>{name}</h3>
         <p className='mt-4 text-secondary text-sm sm:text-xs md:text-sm lg:text-md xl:text-lg 2xl:text-xl'>{description}</p>
       </div>
--- src/data/Project.js
+++ src/data/Project.js
@@ -10,7 +10,7 @@ const projects = [
   {
     name: "Tremor Track",
     description:
-      "Developed during the NASA Space Apps Challenge 2023, Tremor Track, Moonquake Map 2.0 is a 3D interactive lunar globe visualizing seismic activities and geological data. It features timeline navigation, dynamic tooltips, and adjustable visual settings, aimed at making lunar seismic data engaging and educational.",
+      "Developed using during the NASA Space Apps Challenge 2023, Tremor Track, Moonquake Map 2.0 is a 3D interactive lunar globe visualizing seismic activities and geological data. It features timeline navigation, dynamic tooltips, and adjustable visual settings, aimed at making lunar seismic data engaging and educational.",
     video: tremortrack,
   },
   {
@@ -22,27 +22,9 @@ const projects = [
   {
     name: "VBDCSS Intranet",
     description:
-      "Created the network infrastructure and developed an intranet website for the CS Department at Visva-Bharati. Integrated Docker-based microservices, configured a Raspberry Pi 3B+ as a WiFi bridge and DHCP server, and deployed services on an HPE ProLiant DL380 Gen 9 server. Enhanced the department's technological capabilities, including network setup, software deployment, and a secure online exam environment.",
+      "Created infrastructure for an intranet for the CS Department at Visva-Bharati. Integrated Docker-based microservices, configured a custom DHCP server with WiFi bridge, and deployed services on an HPE DL380 server. Enhanced the department's technological capabilities, including network infrastructure and SaaS deployment.",
     image: intranet,
   },
-  /*{
-    name: "TFminiS Library",
-    description:
-      "An Arduino library for interfacing with the Benewake TFmini-S LiDAR sensor, designed for hardware UART communication on ESP32 and Arduino Mega boards. Facilitates reading distance, signal strength, and temperature. Available on the Arduino IDE.",
-    image: tfminis,
-  },
-  {
-    name: "SimpleUltrasonic Library",
-    description:
-      "An official Arduino library for the HC-SR04 ultrasonic sensor, designed to facilitate distance measurements. It provides easy integration with AVR/ESP based MCUs without dependencies. Available on the Arduino IDE.",
-    image: simpleultrasonic,
-  },
-  {
-    name: "Discuss Bot",
-    description:
-      "An AI-powered Python bot that uses the GitHub API and OpenAI's GPT-3.5 Turbo model to generate automatic responses to GitHub discussions. It monitors discussions, fetches topics using the GitHub API, and posts concise, helpful responses. Features include seamless GitHub integration, advanced NLP for understanding and responding to queries, and customizable settings via environment variables and GraphQL.",
-    image: discussbot,
-  },*/
 ];
 
 export { projects };
