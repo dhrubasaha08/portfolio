07f9ea384f3eed57dd21447f6652fc31bdde93b0
Dhruba Saha <dhrubasaha08@gmail.com>
2024-06-11 04:47:58 +0530

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,8 +4,8 @@ import Position from "./Position";
 const Hero = ({ scrollContainer }) => {
   return (
     <section className="parallax">
-      <div className='parallax__content absolute top-[10%] sm:top-[16%] lg:top-[12%] w-full mx-auto lg:pl-[38vh] lg:pr-[30vh] xl:pl-70 xl:pr-72 2xl:px-30 3xl:px-60 flex flex-col lg:flex-row items-start z-10 ml-4'>
-        <div className="flex-1 lg:mb-0">
+      <div className='parallax__content absolute top-[10%] sm:top-[16%] lg:top-[18%] w-full mx-auto lg:pl-[38vh] lg:pr-[30vh] xl:pl-96 xl:pr-72 2xl:px-60 3xl:px-60 flex flex-col lg:flex-row items-start z-10 ml-16'>
+        <div className="flex-1 lg:mb-0 ml--16">
           <h1 className='font-medium text-white text-[40px] xs:text-[50px] sm:text-[68px] md:text-[80px] lg:text-[100px] 2xl:text-[180px] leading-[110px] 2xl:leading-[160px]'>
             Dhruba Saha
           </h1>
